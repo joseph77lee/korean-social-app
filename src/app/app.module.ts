c4fe854d9b0f17475dea62e066cd0b35e11492eb
Joseph's mac <joe@Josephsui-Macmini.local>
2023-05-23 12:41:25 -0600

    Fixed the url in css to be working in build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,7 +45,8 @@ import { ReplyComponent } from './tools/reply/reply.component';
     MatDialogModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
